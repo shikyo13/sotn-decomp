b49abe91833e5cc041ce65c59988cbec252374f3
Adam Hunt <shikyo13@gmail.com>
2026-03-27 16:27:36 -0700

    Decompile func_us_801B77D8 and BO6_RicStepStand in BO6 richter.c
    Decompile 2 matching functions in the BO6 boss overlay's richter.c:
    - func_us_801B77D8: Sets RIC.entityRoomIndex based on relative X position
    - BO6_RicStepStand: Stand step handler with attack/crouch/walk transitions
    
    Also add NON_MATCHING WIP stubs for EntityRichter, BO6_RicStepCrouch,
    and BO6_RicStepHighJump (structurally correct but need register alloc fixes).

--- src/boss/bo6/richter.c
+++ src/boss/bo6/richter.c
@@ -69,13 +69,124 @@ INCLUDE_ASM("boss/bo6/nonmatchings/richter", RichterThinking);
 
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B6998);
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", EntityRichter);
+#else
+extern EInit D_us_80180400;
+extern s32 D_us_801CF3E0;
+extern s32 D_us_801CF3E4;
+extern u16 D_us_801D1686;
+void EntityRichter(Entity* self) {
+    s32 i;
+    Entity* entity;
+    u16 hp;
 
-INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepStand);
+    g_Ric.unk6A = RIC.hitPoints;
+    if (self->step == 0) {
+        InitializeEntity((u16*)&D_us_80180400);
+        func_us_801B4BD0();
+        i = 0x44;
+        entity = &g_Entities[0x44];
+        do {
+            DestroyEntity(entity);
+            i++;
+            entity++;
+        } while (i < 0x90);
+        hp = RIC.hitPoints;
+        D_us_801D1686 = hp;
+        D_us_801CF3E0 = 0;
+        g_Ric.unk6C = hp;
+        g_Ric.unk6A = hp;
+        g_Ric.unk70 = RIC.hitboxState;
+        D_us_801CF3E4 = (u16)D_us_801D1686 >> 1;
+        func_us_801B5A14(0x12);
+        BO6_DisableAfterImage(1, 0x30);
+    } else {
+        RichterThinking();
+        BO6_RicMain();
+        func_us_801BBBD0();
+        func_us_801B6998();
+    }
+    g_Ric.unk6C = g_Ric.unk6A;
+}
+#endif
+
+extern AnimationFrame D_us_80181F1C[];
+extern AnimationFrame D_us_80182130[];
+extern AnimationFrame D_us_801822B8[];
+void BO6_RicStepStand(void) {
+    s32 facing;
+
+    if (!BO6_RicCheckInput(
+            CHECK_FALL | CHECK_FACING | CHECK_JUMP | CHECK_CRASH |
+            CHECK_ATTACK | CHECK_CROUCH | CHECK_SLIDE)) {
+        DecelerateX(FIX(0.125));
+        switch (RIC.step_s) {
+        case 0:
+            if (BO6_RicCheckFacing()) {
+                func_us_801B9DE4(0);
+            } else if (g_Ric.padPressed & PAD_UP) {
+                BO6_RicSetAnimation(D_us_80181F1C);
+                RIC.step_s = 1;
+                break;
+            }
+            break;
+        case 1:
+            if (BO6_RicCheckFacing()) {
+                func_us_801B9DE4(0);
+            } else if (g_Ric.padPressed & PAD_UP) {
+            } else {
+                BO6_RicSetStand(0);
+            }
+            break;
+        case 64:
+            BO6_DisableAfterImage(1, 1);
+            if (RIC.pose < 3) {
+                facing = BO6_RicCheckFacing();
+                if (g_Ric.padPressed & PAD_DOWN) {
+                    RIC.step = PL_S_CROUCH;
+                    RIC.anim = D_us_80182130;
+                    break;
+                }
+            }
+            if (RIC.poseTimer < 0) {
+                if (g_Ric.padPressed & PAD_SQUARE) {
+                    D_us_801D165E = 2;
+                    RIC.step_s++;
+                    BO6_RicSetAnimation(D_us_801822B8);
+                    BO6_RicCreateEntFactoryFromEntity(
+                        g_CurrentEntity, BP_ARM_BRANDISH_WHIP, 0);
+                    break;
+                }
+                D_us_801D165E = 0;
+                BO6_RicSetStand(0);
+            }
+            break;
+        case 65:
+            BO6_DisableAfterImage(1, 1);
+            if (g_Ric.padPressed & PAD_SQUARE) {
+                break;
+            }
+            D_us_801D165E = 0;
+            BO6_RicSetStand(0);
+            break;
+        case 66:
+            BO6_DisableAfterImage(1, 1);
+            if (RIC.pose < 3) {
+                BO6_RicCheckFacing();
+            }
+            if (RIC.poseTimer < 0) {
+                D_us_801D165E = 0;
+                BO6_RicSetStand(0);
+            }
+            break;
+        }
+    }
+}
 
 void BO6_RicStepWalk(void) {
-    if (!BO6_RicCheckInput(CHECK_FALL | CHECK_FACING | CHECK_JUMP | CHECK_CRASH |
-                           CHECK_ATTACK | CHECK_CROUCH)) {
+    if (!BO6_RicCheckInput(CHECK_FALL | CHECK_FACING | CHECK_JUMP |
+                           CHECK_CRASH | CHECK_ATTACK | CHECK_CROUCH)) {
         DecelerateX(FIX(0.125));
         if (BO6_RicCheckFacing() == 0) {
             BO6_RicSetStand(0);
@@ -93,14 +204,13 @@ void BO6_RicStepWalk(void) {
 void BO6_RicStepRun(void) {
     g_Ric.timers[PL_T_8] = 8;
     g_Ric.timers[PL_T_CURSE] = 8;
-    if (!BO6_RicCheckInput(CHECK_FALL | CHECK_FACING | CHECK_JUMP | CHECK_CRASH |
-                           CHECK_ATTACK | CHECK_CROUCH)) {
+    if (!BO6_RicCheckInput(CHECK_FALL | CHECK_FACING | CHECK_JUMP |
+                           CHECK_CRASH | CHECK_ATTACK | CHECK_CROUCH)) {
         DecelerateX(FIX(0.125));
         if (BO6_RicCheckFacing() == 0) {
             BO6_RicSetStand(0);
             if (g_Ric.timers[PL_T_RUN] == 0) {
-                if (!(g_Ric.vram_flag &
-                      (TOUCHING_L_WALL | TOUCHING_R_WALL))) {
+                if (!(g_Ric.vram_flag & (TOUCHING_L_WALL | TOUCHING_R_WALL))) {
                     BO6_RicSetAnimation(D_us_80181F24);
                     BO6_RicCreateEntFactoryFromEntity(
                         g_CurrentEntity, BP_SKID_SMOKE, 0);
@@ -138,7 +248,99 @@ void BO6_RicStepFall(void) {
     }
 }
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepCrouch);
+#else
+extern AnimationFrame D_us_80182048[];
+extern AnimationFrame D_us_80182068[];
+extern AnimationFrame D_us_80182110[];
+extern AnimationFrame D_us_801822C0[];
+void BO6_RicStepCrouch(void) {
+    s32 facing;
+
+    if (!BO6_RicCheckInput(
+            CHECK_FALL | CHECK_FACING | CHECK_ATTACK | CHECK_SLIDE)) {
+        DecelerateX(FIX(0.125));
+        switch (RIC.step_s) {
+        case 0:
+            if (!(g_Ric.padPressed & PAD_DOWN)) {
+                BO6_RicSetAnimation(D_us_80182068);
+                RIC.step_s = 2;
+                return;
+            }
+            break;
+        case 1:
+            if (!(g_Ric.padPressed & PAD_DOWN)) {
+                if (BO6_RicCheckFacing()) {
+                    func_us_801B9DE4(0);
+                    return;
+                }
+                RIC.anim = D_us_80182068;
+                RIC.step_s = 2;
+                RIC.poseTimer = 1;
+                RIC.pose = 2 - RIC.pose;
+                break;
+            }
+        case 4:
+            if (RIC.poseTimer != -1) {
+                return;
+            }
+            BO6_RicSetAnimation(D_us_80182048);
+            RIC.step_s = 0;
+            break;
+        case 2:
+            if (BO6_RicCheckFacing()) {
+                func_us_801B9DE4(0);
+                return;
+            }
+            if (RIC.poseTimer == -1) {
+                BO6_RicSetStand(0);
+                return;
+            }
+            break;
+        case 3:
+            if (RIC.poseTimer < 0) {
+                BO6_RicSetAnimation(D_us_80182048);
+                RIC.step_s = 0;
+                return;
+            }
+            break;
+        case 0x40:
+            BO6_DisableAfterImage(1, 1);
+            if (RIC.pose < 3) {
+                facing = BO6_RicCheckFacing();
+                if (!(g_Ric.padPressed & PAD_DOWN)) {
+                    RIC.step = PL_S_STAND;
+                    RIC.anim = D_us_80182110;
+                    return;
+                }
+            }
+            if (RIC.poseTimer < 0) {
+                if (g_Ric.padPressed & PAD_SQUARE) {
+                    D_us_801D165E = 2;
+                    RIC.step_s++;
+                    BO6_RicSetAnimation(D_us_801822C0);
+                    BO6_RicCreateEntFactoryFromEntity(
+                        g_CurrentEntity, BP_ARM_BRANDISH_WHIP, 0);
+                    return;
+                }
+                D_us_801D165E = 0;
+                RIC.step_s = 0;
+                BO6_RicSetAnimation(D_us_80182048);
+            }
+            break;
+        case 0x41:
+            BO6_DisableAfterImage(1, 1);
+            if (!(g_Ric.padPressed & PAD_SQUARE)) {
+                D_us_801D165E = 0;
+                RIC.step_s = 0;
+                BO6_RicSetAnimation(D_us_80182048);
+            }
+            break;
+        }
+    }
+}
+#endif
 
 void BO6_RicResetPose(void) {
     RIC.pose = RIC.poseTimer = 0;
@@ -147,7 +349,16 @@ void BO6_RicResetPose(void) {
     RIC.drawFlags &= ~ENTITY_ROTATE;
 }
 
-INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B77D8);
+void func_us_801B77D8(void) {
+    s32 diff;
+
+    diff = RIC.posX.i.hi - PLAYER.posX.i.hi;
+    if (diff <= 0) {
+        RIC.entityRoomIndex = 0;
+    } else {
+        RIC.entityRoomIndex = 1;
+    }
+}
 
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepHit);
 
@@ -268,4 +479,52 @@ void func_us_801B8E80(u16 arg0) {
     }
 }
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepHighJump);
+#else
+void BO6_RicStepHighJump(void) {
+    s32 setFall;
+
+    D_us_801D165A++;
+    setFall = 0;
+    switch (RIC.step_s) {
+    case 0:
+        if ((g_Ric.padPressed & (PAD_LEFT | PAD_DOWN)) &&
+            (RIC.facingLeft ? (g_Ric.padPressed & PAD_LEFT)
+                            : (g_Ric.padPressed & PAD_DOWN))) {
+        } else {
+            DecelerateX(FIX(0.25));
+        }
+        if (g_Ric.vram_flag & TOUCHING_CEILING) {
+            func_us_801B8E80(3);
+            D_us_801D165A = 0;
+            RIC.step_s = 2;
+        } else if (D_us_801D165A >= 0x1D) {
+            RIC.step_s = 1;
+            RIC.velocityY = FIX(-6);
+        }
+        break;
+    case 1:
+        if (g_Ric.vram_flag & TOUCHING_CEILING) {
+            RIC.step_s = 2;
+            func_us_801B8E80(3);
+            D_us_801D165A = 0;
+        } else {
+            RIC.velocityY += FIX(1.5);
+            if (FIX(2) < RIC.velocityY) {
+                setFall = 1;
+            }
+        }
+        break;
+    case 2:
+        if (D_us_801D165A >= 5) {
+            setFall = 1;
+        }
+        break;
+    }
+    if (setFall) {
+        BO6_RicSetAnimation(D_us_801820BC);
+        BO6_RicSetStep(PL_S_JUMP);
+    }
+}
+#endif
