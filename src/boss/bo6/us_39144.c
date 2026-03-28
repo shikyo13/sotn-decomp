8d395bc8f896fea066e5b0243dd2b9c0b5333a65
Adam Hunt <shikyo13@gmail.com>
2026-03-28 07:22:43 -0700

    Decompile 3 entity handlers in BO6 us_39144.c (SHA1 fe067af9 verified)
    - func_us_801BC2F0 (slide hitbox entity, tracks RIC position)
    - func_us_801BC3E0 (slide kick hitbox entity, adjusts hitbox per frame)
    - func_us_801BC5C8 (high jump hitbox entity)

--- src/boss/bo6/us_39144.c
+++ src/boss/bo6/us_39144.c
@@ -17,6 +17,8 @@ extern AnimationFrame D_us_80182094[];
 extern AnimationFrame D_us_80182324[];
 extern AnimationFrame D_us_80182360[];
 extern EInit D_us_8018043C;
+extern EInit D_us_80180448;
+extern EInit D_us_80180430;
 
 INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", func_us_801B9144);
 
@@ -407,11 +409,56 @@ void func_us_801BC2F0(Entity* self) {
     }
 }
 
-INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", func_us_801BC3E0);
+void func_us_801BC3E0(Entity* self) {
+    if (RIC.step != PL_S_SLIDE_KICK) {
+        DestroyEntity(self);
+        return;
+    }
+    self->posX.i.hi = RIC.posX.i.hi;
+    self->posY.i.hi = RIC.posY.i.hi;
+    self->facingLeft = RIC.facingLeft;
+    if (!self->step) {
+        InitializeEntity(&D_us_80180448);
+        self->flags = 0x18000000;
+        self->hitboxOffX = 0x14;
+        self->hitboxWidth = 9;
+        self->hitboxHeight = 9;
+        self->step = 1;
+    }
+    if (RIC.animCurFrame == 0x8C) {
+        self->hitboxOffY = 0;
+    }
+    if (RIC.animCurFrame == 0x8D) {
+        self->hitboxOffY = 0xC;
+    }
+    if (self->hitFlags) {
+        g_Ric.unk44 |= 0x80;
+    } else {
+        g_Ric.unk44 &= ~0x80;
+    }
+    self->hitFlags = 0;
+}
 
 INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", func_us_801BC4F8);
 
-INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", func_us_801BC5C8);
+void func_us_801BC5C8(Entity* self) {
+    if (RIC.step != PL_S_HIGHJUMP) {
+        DestroyEntity(self);
+        return;
+    }
+    self->posX.i.hi = RIC.posX.i.hi;
+    self->posY.i.hi = RIC.posY.i.hi;
+    self->facingLeft = RIC.facingLeft;
+    if (!self->step) {
+        InitializeEntity(&D_us_80180430);
+        self->flags = 0x18000000;
+        self->hitboxOffX = 0xC;
+        self->hitboxOffY = -0x1A;
+        self->hitboxWidth = 0xC;
+        self->hitboxHeight = 0xC;
+        self->step = 1;
+    }
+}
 
 INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", func_us_801BC678);
 
