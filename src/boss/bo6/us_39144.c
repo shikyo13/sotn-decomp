a8fb5c81db240779286cbb6eec6d46b2d5e1ca60
Adam Hunt <shikyo13@gmail.com>
2026-03-28 13:05:34 -0700

    Decompile BO6_RicEntityHitByHoly in BO6 us_39144.c (SHA1 516fcf6d verified)
    Analog of RicEntityHitByHoly in pl_blueprints.c. Holy water hit effect
    with 16 rising particle primitives.

--- src/boss/bo6/us_39144.c
+++ src/boss/bo6/us_39144.c
@@ -21,6 +21,7 @@ extern EInit D_us_8018043C;
 extern EInit D_us_80180448;
 extern AnimationFrame D_us_80181554[];
 extern AnimationFrame D_us_80181A40[];
+extern Point16 D_us_801D080C[16];
 extern s32 D_us_801D084C;
 extern EInit D_us_80180424;
 extern EInit D_us_80180430;
@@ -924,7 +925,83 @@ s32 func_us_801BB5BC(Primitive* prim, s16 posX, s16 posY) {
     return ret;
 }
 
-INCLUDE_ASM("boss/bo6/nonmatchings/us_39144", BO6_RicEntityHitByHoly);
+void BO6_RicEntityHitByHoly(Entity* entity) {
+    Primitive* prim;
+    s32 i;
+    s32 temp;
+    s16 hitboxX;
+    s16 hitboxY;
+    s16 temp_xRand;
+    s16 temp_yRand;
+
+    switch (entity->step) {
+    case 0:
+        entity->primIndex =
+            (s16)g_api.AllocPrimitives(PRIM_GT4, 16);
+        if (entity->primIndex == -1) {
+            DestroyEntity(entity);
+            return;
+        }
+        entity->flags = FLAG_HAS_PRIMS | FLAG_POS_CAMERA_LOCKED;
+        hitboxX = RIC.posX.i.hi + RIC.hitboxOffX;
+        hitboxY = RIC.posY.i.hi + RIC.hitboxOffY;
+        prim = &g_PrimBuf[entity->primIndex];
+        for (i = 0; i < 16; i++) {
+            temp_xRand = hitboxX + rand() % 24 - 12;
+            temp_yRand = hitboxY + rand() % 48 - 24;
+            D_us_801D080C[i].x = temp_xRand;
+            D_us_801D080C[i].y = temp_yRand;
+            prim->clut = PAL_UNK_1B2;
+            prim->tpage = 0x1A;
+            prim->b0 = 0;
+            prim->b1 = 0;
+            prim->g0 = 0;
+            prim->g1 = (rand() & 7) + 1;
+            prim->g2 = 0;
+            prim->priority = RIC.zPriority + 4;
+            prim->drawMode = DRAW_UNK_100 | DRAW_TPAGE | DRAW_HIDE |
+                             DRAW_UNK02 | DRAW_TRANSP;
+            if (rand() & 1) {
+                prim->drawMode =
+                    DRAW_UNK_100 | DRAW_UNK_40 | DRAW_TPAGE2 | DRAW_TPAGE |
+                    DRAW_HIDE | DRAW_UNK02 | DRAW_TRANSP;
+            }
+            prim = prim->next;
+        }
+        entity->step++;
+        break;
+    case 1:
+        if (!(g_Ric.status & PLAYER_STATUS_UNK10000)) {
+            DestroyEntity(entity);
+            return;
+        }
+        break;
+    }
+
+    prim = &g_PrimBuf[entity->primIndex];
+    for (i = 0; i < 16; i++) {
+        switch (prim->g0) {
+        case 0:
+            if (--prim->g1 == 0) {
+                prim->g0++;
+            }
+            break;
+        case 1:
+            hitboxX = D_us_801D080C[i].x;
+            hitboxY = D_us_801D080C[i].y;
+            temp = func_us_801BB5BC(prim, hitboxX, hitboxY);
+            D_us_801D080C[i].y--;
+            if (temp < 0) {
+                prim->drawMode |= DRAW_HIDE;
+                prim->g0++;
+            } else {
+                prim->drawMode &= ~DRAW_HIDE;
+            }
+            break;
+        }
+        prim = prim->next;
+    }
+}
 
 void BO6_RicEntityHitByDark(Entity* entity) {
     switch (entity->step) {
